9753ca29878d3fae5a8192a9938d6974d0337839
Kedar Hargude <hargude481@gmail.com>
2022-05-11 09:53:47 +0530

    Time: 0 ms (100.00%), Space: 46.4 MB (44.63%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -3,19 +3,22 @@ class Solution {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end ){
+        while(start < end ){
             int mid = start + (end-start)/2;
 
-            if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
-                start = mid;
-            }
-            else if (arr[mid] < arr[mid-1] && arr[mid] > arr[mid+1]){
+            //we will only be checking mid and (mid+1) values.
+            if(arr[mid] > arr[mid+1]){
+                //descending part of the array
+                //this mid can be the answer, but look at left too. So, end!= mid-1...we may miss
+                // the answer if it is the end value
                 end = mid;
             }
-            else if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
-                return mid;
+            else{
+                //ascending part of the array
+                start = mid+1;
+                // as we know mid+1 is greater than mid...so why waste it.
             }
         }
-        return -1;
+        return start;
     }
 }
